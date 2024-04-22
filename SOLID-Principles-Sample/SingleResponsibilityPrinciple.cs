b53fbcd9680c80dc0f4e2cbf923f01016c1570dc
abhirampadhi <abhiraampadhi@outlook.com>
2024-04-22 13:34:58 +0200

    SRP

---
+++ SOLID-Principles-Sample/SingleResponsibilityPrinciple.cs
@@ -0,0 +1,30 @@
+﻿// Bad example violating SRP
+public class Customer
+{
+    public void AddCustomer()
+    {
+        // Code to add a customer to the database
+    }
+
+    public void SendEmail()
+    {
+        // Code to send an email to the customer
+    }
+}
+
+// Good example following SRP
+public class CustomerService
+{
+    public void AddCustomer()
+    {
+        // Code to add a customer to the database
+    }
+}
+
+public class EmailService
+{
+    public void SendEmail()
+    {
+        // Code to send an email to the customer
+    }
+}
