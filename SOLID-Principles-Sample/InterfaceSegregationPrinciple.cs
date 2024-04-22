21154a54d225f019bcb158879ec9a8225368e3d9
abhirampadhi <abhiraampadhi@outlook.com>
2024-04-22 16:18:58 +0200

    OLID Principles

---
+++ SOLID-Principles-Sample/InterfaceSegregationPrinciple.cs
@@ -0,0 +1,69 @@
+﻿// Bad example violating ISP
+public interface IWorker
+{
+    void Work();
+    void Eat();
+    void Sleep();
+}
+
+public class Robot : IWorker
+{
+    public void Work()
+    {
+        // Code for performing work
+    }
+
+    public void Eat()
+    {
+        // Robots don't eat!
+        throw new NotSupportedException();
+    }
+
+    public void Sleep()
+    {
+        // Robots don't sleep!
+        throw new NotSupportedException();
+    }
+}
+
+// Good example following ISP
+public interface IWorker
+{
+    void Work();
+}
+
+public interface IEater
+{
+    void Eat();
+}
+
+public interface ISleeper
+{
+    void Sleep();
+}
+
+public class Human : IWorker, IEater, ISleeper
+{
+    public void Work()
+    {
+        // Code for performing work
+    }
+
+    public void Eat()
+    {
+        // Code for eating
+    }
+
+    public void Sleep()
+    {
+        // Code for sleeping
+    }
+}
+
+public class Robot : IWorker
+{
+    public void Work()
+    {
+        // Code for performing work
+    }
+}
---
+++ SOLID-Principles-Sample/LiskovSubstitutionPrinciple.cs
@@ -0,0 +1,69 @@
+﻿// Bad example violating LSP
+public class Rectangle
+{
+    public int Width { get; set; }
+    public int Height { get; set; }
+
+    public virtual void SetWidth(int width)
+    {
+        Width = width;
+    }
+
+    public virtual void SetHeight(int height)
+    {
+        Height = height;
+    }
+
+    public int CalculateArea()
+    {
+        return Width * Height;
+    }
+}
+
+public class Square : Rectangle
+{
+    public override void SetWidth(int width)
+    {
+        Width = width;
+        Height = width;
+    }
+
+    public override void SetHeight(int height)
+    {
+        Width = height;
+        Height = height;
+    }
+}
+
+// Good example following LSP
+public abstract class Shape
+{
+    public abstract int CalculateArea();
+}
+
+public class Rectangle : Shape
+{
+    public int Width { get; set; }
+    public int Height { get; set; }
+
+    public override int CalculateArea()
+    {
+        return Width * Height;
+    }
+}
+
+public class Square : Shape
+{
+    public int SideLength { get; set; }
+
+    public override int CalculateArea()
+    {
+        return SideLength * SideLength;
+    }
+}
+
+//Rectangle rectangle = new Square();
+//rectangle.SetWidth(5);
+//rectangle.SetHeight(3);
+
+//int area = rectangle.CalculateArea(); // Expected result: 15, Actual result: 9
