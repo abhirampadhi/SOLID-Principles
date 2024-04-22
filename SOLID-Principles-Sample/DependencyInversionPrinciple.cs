4c04773365620d9300450a6321391be36c4fe25d
abhirampadhi <abhiraampadhi@outlook.com>
2024-04-22 16:18:58 +0200

    OLID Principles

---
+++ SOLID-Principles-Sample/DependencyInversionPrinciple.cs
@@ -0,0 +1,56 @@
+﻿// Bad example violating DIP
+public class DataAccess
+{
+    public void SaveData(string data)
+    {
+        // Code to save data to a specific database
+    }
+}
+
+public class UserService
+{
+    private readonly DataAccess _dataAccess;
+
+    public UserService()
+    {
+        _dataAccess = new DataAccess();
+    }
+
+    public void CreateUser(string username, string password)
+    {
+        // Code to create a user
+
+        _dataAccess.SaveData("User created: " + username);
+    }
+}
+
+// Good example following DIP
+public interface IDataAccess
+{
+    void SaveData(string data);
+}
+
+public class DataAccess : IDataAccess
+{
+    public void SaveData(string data)
+    {
+        // Code to save data to a specific database
+    }
+}
+
+public class UserService
+{
+    private readonly IDataAccess _dataAccess;
+
+    public UserService(IDataAccess dataAccess)
+    {
+        _dataAccess = dataAccess;
+    }
+
+    public void CreateUser(string username, string password)
+    {
+        // Code to create a user
+
+        _dataAccess.SaveData("User created: " + username);
+    }
+}
---
+++ SOLID-Principles-Sample/OpenClosedPrinciple.cs
@@ -0,0 +1,52 @@
+﻿// Bad example violating OCP
+public enum VehicleType
+{
+    Car,
+    Motorcycle
+}
+
+public class Vehicle
+{
+    public VehicleType Type { get; set; }
+
+    public double CalculateInsurancePremium()
+    {
+        double basePremium = 1000;
+
+        // Calculate insurance premium based on vehicle type
+        if (Type == VehicleType.Car)
+        {
+            return basePremium * 1.5;
+        }
+        else if (Type == VehicleType.Motorcycle)
+        {
+            return basePremium * 2;
+        }
+
+        return basePremium;
+    }
+}
+
+// Good example following OCP
+public abstract class Vehicle
+{
+    public abstract double CalculateInsurancePremium();
+}
+
+public class Car : Vehicle
+{
+    public override double CalculateInsurancePremium()
+    {
+        double basePremium = 1000;
+        return basePremium * 1.5;
+    }
+}
+
+public class Motorcycle : Vehicle
+{
+    public override double CalculateInsurancePremium()
+    {
+        double basePremium = 1000;
+        return basePremium * 2;
+    }
+}
